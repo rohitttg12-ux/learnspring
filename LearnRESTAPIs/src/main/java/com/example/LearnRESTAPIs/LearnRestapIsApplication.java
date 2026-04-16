f5f821b582c85f5f5d2cfa05776452caaffd792a
rohitttg12-ux <rohitttg12@gmail.com>
2026-04-16 19:01:32 +0530

    Add files via upload

---
+++ LearnRESTAPIs/src/main/java/com/example/LearnRESTAPIs/LearnRestapIsApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnRESTAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRestapIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRestapIsApplication.class, args);
+	}
+
+}
---
+++ LearnRESTAPIs/src/test/java/com/example/LearnRESTAPIs/LearnRestapIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnRESTAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnRestapIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
